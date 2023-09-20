a95d590b75c43d8111c9c0a04587e1cac2739708
Oliver Larsen <oliverlarsen@MacBook-Air-tilhrende-Oliver.local>
2023-09-20 20:50:53 +0200

    pfCorrectionsAndSomeTests

--- test.sh
+++ test.sh
@@ -12,12 +12,24 @@ rm -f test_files/*
 
 
 echo "Generating test files.."
+# Ascii
 printf "Hello, World!\n" > test_files/ascii.input
 printf "Hello, World!" > test_files/ascii2.input
-printf "Hello,\x00World!\n" > test_files/data.input
-printf "" > test_files/empty.input
-printf "This is \377n iso file" > test_files/iso.input
-printf "This is UTF because of the character: 你好" > test_files/utf.input
+printf "\x06 is not interval \x06" > test_files/ascii3.input
+# printf "a \n lot \n of \n newlines" > test_files/ascii3.input
+# printf " spaces     " > test_files/ascii4.input
+# printf "Hello there escape \x1B" > test_files/ascii5.input
+# printf "\x1A is not in the interval" > test_files/ascii6.input
+# printf "109 is a number \x1B" > test_files/ascii7.input
+# ISO-8859
+# printf "This is \377n iso file" > test_files/iso.input
+# # UTF-8
+# printf "This is UTF because of the character: 你好" > test_files/utf.input
+# # Empty
+# printf "" > test_files/empty.input
+# # Data
+# printf "Hello,\x00World!\n" > test_files/data.input
+# printf "0"
 ### TODO: Generate more test files ###
 
 
