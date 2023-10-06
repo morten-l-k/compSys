cbbfa05c6839b57fc3427042c52aa091c04fd906
morten-l-k <kgt837@alumni.ku.dk>
2023-10-06 09:47:23 +0200

    Added coord_query_naive

--- A1/coord_query_naive.c
+++ A1/coord_query_naive.c
@@ -5,6 +5,7 @@
 #include <stdint.h>
 #include <errno.h>
 #include <assert.h>
+#include <math.h>
 
 #include "record.h"
 #include "coord_query.h"
@@ -15,18 +16,33 @@ struct naive_data {
 };
 
 struct naive_data* mk_naive(struct record* rs, int n) {
-  assert(0);
-  // TODO
+    struct naive_data *nd = malloc(sizeof(struct naive_data));
+    nd->rs = rs;
+    nd->n = n;
+    return nd;
 }
 
 void free_naive(struct naive_data* data) {
-  assert(0);
-  // TODO
+  free(data);
+}
+
+double distance(double lon, double lat, double test_lon, double test_lat) {
+    return sqrt(pow(lon - test_lon,2) + pow(lat - test_lat, 2));
 }
 
 const struct record* lookup_naive(struct naive_data *data, double lon, double lat) {
-  assert(0);
-  // TODO
+    struct record *result = &(data->rs[0]);
+    double shortest_distance = distance(lon,lat,result->lon,result->lat);
+
+    for (int i = 1; i < data->n; i++) {
+        double test_distance = distance(lon,lat,data->rs[i].lon,data->rs[i].lat);
+        if (test_distance < shortest_distance) {
+            result = &(data->rs[i]);
+            shortest_distance = test_distance;
+        }
+    }
+    
+    return result;
 }
 
 int main(int argc, char** argv) {
