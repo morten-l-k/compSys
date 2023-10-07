e8fc145ff8cdd423c46dcf3061d9b02b62265c1a
morten-l-k <kgt837@alumni.ku.dk>
2023-10-07 11:42:32 +0200

    Updated formatting in id_query_bonus

--- A1/id_query_bonus.c
+++ A1/id_query_bonus.c
@@ -9,29 +9,24 @@
 #include "record.h"
 #include "id_query.h"
 
-struct treeNode
-{
+struct treeNode {
     int64_t osm_id;
     struct record * value;
     struct treeNode *left;
     struct treeNode *right;
 };
 
-int compare_index_records(const void *a, const void *b)
-{
+int compare_index_records(const void *a, const void *b) {
     const struct record *record_a = (const struct record *)a;
     const struct record *record_b = (const struct record *)b;
 
-    if (record_a->osm_id < record_b->osm_id)
-    {
+    if (record_a->osm_id < record_b->osm_id) {
         return -1;
     }
-    else if (record_a->osm_id > record_b->osm_id)
-    {
+    else if (record_a->osm_id > record_b->osm_id) {
         return 1;
     }
-    else
-    {
+    else {
         return 0;
     }
 }
@@ -50,41 +45,39 @@ struct treeNode* construct_treeNode(struct treeNode* parent, struct record *r){
     return parent;
 }
 
-struct treeNode *mk_indexed(struct record *rs, int n)
-{
+struct treeNode *mk_indexed(struct record *rs, int n) {
     struct treeNode* root = NULL;
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         root = construct_treeNode(root,&rs[i]);
     }
     
     return root;
 }
 
-void free_indexed(struct treeNode *data)
-{
-    if (data != NULL)
-    {
+void free_indexed(struct treeNode *data) {
+    if (data != NULL) {
         free_indexed(data->right);
         free_indexed(data->left);
         free(data);
     }
 }
 
-const struct record *lookup_binary(struct treeNode *data, int64_t needle)
-{
-    if(data == NULL)
+const struct record *lookup_binary(struct treeNode *data, int64_t needle) {
+    if(data == NULL) {
         return NULL;
-    if(data -> osm_id == needle)
+    }
+    if(data -> osm_id == needle) {
         return data->value;
-    if(data->osm_id > needle)
+    }
+    if(data->osm_id > needle) {
         return lookup_binary(data->left,needle);
-    else
+    }
+    else {
         return lookup_binary(data->right,needle);
+    }
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     return id_query_loop(argc, argv,
                          (mk_index_fn)mk_indexed,
                          (free_index_fn)free_indexed,
