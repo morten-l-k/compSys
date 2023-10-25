55ca18b4056ca46ebbe59caad20805582bd5912f
Josefine <flw772@alumni.ku.dk>
2023-10-25 17:58:57 +0200

    Made fhistogram-mt

--- A2/fhistogram-mt.c
+++ A2/fhistogram-mt.c
@@ -21,6 +21,60 @@ pthread_mutex_t stdout_mutex = PTHREAD_MUTEX_INITIALIZER;
 #include <err.h>
 
 #include "histogram.h"
+int global_histogram[8] = { 0 };
+// Opretter en mutex (lås) og initialiserer den. Dette forhindrer konkurrerende
+// tråde i at samtidig opdatere histogrammet, hvilket sikrer trådsikkerhed.
+pthread_mutex_t global_histogram_mutex;
+
+
+int fhistogram(char const *path) {
+  FILE *f = fopen(path, "r");
+
+  int local_histogram[8] = { 0 };
+
+  if (f == NULL) {
+    fflush(stdout);
+    warn("failed to open %s", path);
+    return -1;
+  }
+
+  int i = 0;
+
+  char c;
+  while (fread(&c, sizeof(c), 1, f) == 1) {
+    i++;
+    update_histogram(local_histogram, c);
+    if ((i % 100000) == 0) {
+      pthread_mutex_lock(&global_histogram_mutex);
+      merge_histogram(local_histogram, global_histogram);
+      print_histogram(global_histogram);
+      pthread_mutex_unlock(&global_histogram_mutex);
+    }
+  }
+
+  fclose(f);
+  pthread_mutex_lock(&global_histogram_mutex);
+  merge_histogram(local_histogram, global_histogram);
+  print_histogram(global_histogram);
+  pthread_mutex_unlock(&global_histogram_mutex);
+  return 0;
+}
+
+void *worker(void *arg) {
+  struct job_queue *queue = arg;
+  char *path;
+  while (1) {
+    // Hent en opgave (Job) fra jobkøen. Hvis der ikke er flere opgaver, afslut tråden.
+      job_queue_pop(queue,(void**)&path);
+      if (path == NULL) {
+          break;
+      }
+      fhistogram(path);
+  }
+  free(path);
+  return NULL;
+}
+
 
 int main(int argc, char * const *argv) {
   if (argc < 2) {
@@ -49,7 +103,15 @@ int main(int argc, char * const *argv) {
     paths = &argv[1];
   }
 
-  assert(0); // Initialise the job queue and some worker threads here.
+    // Create job queue.
+  struct job_queue queue;
+  job_queue_init(&queue, 64);
+
+  //mutex (lås)
+    if (pthread_mutex_init(&global_histogram_mutex, NULL) != 0) {
+        perror("Mutex initialization failed");
+        exit(1);
+    }
 
   // FTS_LOGICAL = follow symbolic links
   // FTS_NOCHDIR = do not change the working directory of the process
@@ -64,18 +126,33 @@ int main(int argc, char * const *argv) {
     return -1;
   }
 
+  // Start up the worker threads.
+  pthread_t *threads = calloc(num_threads, sizeof(pthread_t));
+  for (int i = 0; i < num_threads; i++) {
+    if (pthread_create(&threads[i], NULL, &worker, &queue) != 0) {
+      err(1, "pthread_create() failed");
+    }
+  }
+
   FTSENT *p;
   while ((p = fts_read(ftsp)) != NULL) {
     switch (p->fts_info) {
     case FTS_D:
       break;
     case FTS_F:
-      assert(0); // Process the file p->fts_path, somehow.
+      job_queue_push(&queue,(void*)strdup(p->fts_path)); // lig i queue
       break;
     default:
       break;
     }
   }
+  // // Wait for all threads to finish.  This is important, as some may
+  // // still be working on their job.
+    for (int i = 0; i < num_threads; i++) {
+        if (pthread_join(threads[i], NULL) != 0) {
+            err(1, "pthread_join() failed");
+        }
+    }
 
   fts_close(ftsp);
 
